f8da860d46af389d537f06466b9daf32149aa6ad
vemanritti007 <vemanritti007@gmail.com>
2026-08-23 14:46:40 +0000

    Sync LeetCode submission - Find All Anagrams in a String (java)

--- problems/find_all_anagrams_in_a_string/solution.java
+++ problems/find_all_anagrams_in_a_string/solution.java
@@ -1,26 +1,49 @@
 class Solution {
+
+    public static boolean compare(int a[], int b[]) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i])
+                return false;
+        }
+        return true;
+    }
+
     public List<Integer> findAnagrams(String s, String p) {
+
         List<Integer> ans = new ArrayList<>();
-        HashMap<Character,Integer> sm = new HashMap<>();
-        HashMap<Character,Integer> pm = new HashMap<>();
-        for(char c : p.toCharArray())
-        {
-            pm.put(c,pm.getOrDefault(c,0)+1);
+
+        if (p.length() > s.length())
+            return ans;
+
+        int freq[] = new int[26];
+
+        for (int i = 0; i < p.length(); i++) {
+            freq[p.charAt(i) - 'a']++;
         }
-        int left=0,count=p.length();
-        for(int right=0;right<s.length();right++)
-        {
-            char c = s.charAt(right);
-            sm.put(c,sm.getOrDefault(c,0)+1);
-            if(pm.containsKey(c) && sm.get(c)<=pm.get(c))count--;
-            if(right-left+1>p.length()){
-                char lc = s.charAt(left);
-                if(pm.containsKey(lc) && sm.get(lc)<=pm.get(lc))count++;
-                sm.put(lc,sm.get(lc)-1);
-                left++;
-            }
-            if(count==0)ans.add(left);
+
+        int n = p.length();
+        int window[] = new int[26];
+
+        for (int i = 0; i < n; i++) {
+            window[s.charAt(i) - 'a']++;
         }
+
+        if (compare(freq, window))
+            ans.add(0);
+
+        int right = n;
+
+        while (right < s.length()) {
+
+            window[s.charAt(right) - 'a']++;
+            window[s.charAt(right - n) - 'a']--;
+
+            if (compare(freq, window))
+                ans.add(right - n + 1);
+
+            right++;
+        }
+
         return ans;
     }
 }
