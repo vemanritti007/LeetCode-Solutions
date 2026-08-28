3cec725c6466c206a7a5b82160582ee4352f0dda
vemanritti007 <vemanritti007@gmail.com>
2026-08-28 12:56:28 +0000

    Sync LeetCode submission - Continuous Subarray Sum (java)

---
+++ problems/continuous_subarray_sum/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+      int sum=0;
+      HashMap<Integer,Integer> map = new HashMap<>();
+      map.put(0,-1);
+      for(int i=0;i<nums.length;i++){
+        sum+=nums[i];
+        int rem=sum%k;
+        if(map.containsKey(rem)){
+            if(i-map.get(rem)>=2){
+            return true;
+            }
+        }
+        else{
+        map.put(rem,i);
+        }
+      }
+      return false; 
+    }
+}
