8ce21d9127006c54f080a3d246e73a6e48d12eb9
vemanritti007 <vemanritti007@gmail.com>
2026-09-06 07:02:56 +0000

    Sync LeetCode submission - Maximum Subarray (java)

--- problems/maximum_subarray/solution.java
+++ problems/maximum_subarray/solution.java
@@ -1,19 +1,12 @@
+//kadanes algorithm if upon adding current element makes the sum 0 then ignore that subarray and start a fresh one.
 class Solution {
-    public int maxSubArray(int[] nums) 
-    {
-    int currSum=0;
-    int sum=Integer.MIN_VALUE;
-    for(int i=0;i<nums.length;i++)
-    {
-        currSum+=nums[i];
-        if(currSum>sum)
-        {
-            sum=currSum;
+    public int maxSubArray(int[] nums) {
+        int currS=0,sum=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            currS+=nums[i];
+            sum = (currS>sum)?currS:sum;
+            currS=(currS>0)?currS:0;
         }
-        if(currSum<0){
-            currSum=0;
-        }
-    }
-    return sum;
+        return sum;
     }
 }
