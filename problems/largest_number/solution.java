9547b39a2c2cf5658776ec94f74d12f2ff605a99
vemanritti007 <vemanritti007@gmail.com>
2026-09-14 14:44:07 +0000

    Sync LeetCode submission - Largest Number (java)

--- problems/largest_number/solution.java
+++ problems/largest_number/solution.java
@@ -1,13 +1,25 @@
 class Solution {
-    public String largestNumber(int[] nums) {
-        String num[] = new String[nums.length];
-        for(int i=0;i<nums.length;i++){
-            num[i]=String.valueOf(nums[i]);
+    public static int compareChar(String a , String b){
+        int la = a.length(),lb=b.length();
+        for(int i=0;i<la+lb;i++){
+            char ca = (i<la)?a.charAt(i):b.charAt(i-la);
+            char cb = (i<lb)?b.charAt(i):a.charAt(i-lb);
+            if(ca!=cb) return cb-ca;
         }
-        Arrays.sort(num,(a,b)->(b+a).compareTo(a+b));
-        if(num[0].equals("0"))return "0";
-        StringBuilder sb = new StringBuilder();
-        for(String s : num){
+        return 0;
+    }
+
+    public String largestNumber(int[] nums) {
+        String arr[] = new String[nums.length];
+        int totallen=0;
+        for(int i=0;i<nums.length;i++){
+            arr[i] = Integer.toString(nums[i]);
+            totallen+=arr[i].length();
+        }
+        Arrays.sort(arr,Solution::compareChar);
+        if(arr[0].charAt(0)=='0') return "0";
+        StringBuilder sb = new StringBuilder(totallen);
+        for(String s : arr){
             sb.append(s);
         }
         return sb.toString();
