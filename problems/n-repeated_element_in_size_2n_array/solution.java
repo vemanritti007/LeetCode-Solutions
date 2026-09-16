e10762049eb8754b45215b5ac25d910f4f3856a0
vemanritti007 <vemanritti007@gmail.com>
2026-09-16 14:00:59 +0000

    Sync LeetCode submission - N-Repeated Element in Size 2N Array (java)

---
+++ problems/n-repeated_element_in_size_2n_array/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(hs.contains(nums[i])) return nums[i];
+            hs.add(nums[i]);
+        }
+        return 0;
+    }
+}
