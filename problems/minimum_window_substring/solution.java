2c546a1350621c6b7c662a50448b378652509f04
vemanritti007 <vemanritti007@gmail.com>
2026-09-21 10:37:17 +0000

    Sync LeetCode submission - Minimum Window Substring (java)

--- problems/minimum_window_substring/solution.java
+++ problems/minimum_window_substring/solution.java
@@ -1,41 +1,28 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) return "";
-
-        int[] need = new int[128];
-        int required = 0; // distinct chars needed
-        for (char c : t.toCharArray()) {
-            if (need[c] == 0) required++;
+        if(t.length() > s.length())return "";
+        int formed =0,required=0,minLen=Integer.MAX_VALUE,minStart=0,left=0;
+        int need[] = new int[128];
+        int window[] = new int[128];
+        for(char c : t.toCharArray()){
+            if(need[c]==0)required++;
             need[c]++;
         }
-
-        int[] window = new int[128];
-        int formed = 0;
-        int left = 0;
-        int minLen = Integer.MAX_VALUE;
-        int minStart = 0;
-
-        for (int right = 0; right < s.length(); right++) {
+        for(int right=0;right<s.length();right++){
             char rc = s.charAt(right);
             window[rc]++;
-            if (need[rc] > 0 && window[rc] == need[rc]) {
-                formed++;
-            }
-
-            while (formed == required) {
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
-                    minStart = left;
+            if(need[rc]>0 && window[rc]==need[rc])formed++;
+            while(formed==required){
+                if(minLen>right-left+1){
+                    minLen=right-left+1;
+                    minStart=left;
                 }
                 char lc = s.charAt(left);
                 window[lc]--;
-                if (need[lc] > 0 && window[lc] < need[lc]) {
-                    formed--;
-                }
+                if(need[lc]>0 && window[lc]<need[lc])formed--;
                 left++;
             }
         }
-
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
+        return minLen==Integer.MAX_VALUE?"":s.substring(minStart,minLen+minStart);
     }
 }
