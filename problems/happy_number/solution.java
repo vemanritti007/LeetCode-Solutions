69adacf9398e115ec74ff6372b469db8e0ff7501
vemanritti007 <vemanritti007@gmail.com>
2026-09-22 18:15:40 +0000

    Sync LeetCode submission - Happy Number (java)

---
+++ problems/happy_number/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isHappy(int n) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        while (n != 1) {
+
+            if (set.contains(n))
+                return false;
+
+            set.add(n);
+
+            int sum = 0;
+
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+
+            n = sum;
+        }
+
+        return true;
+    }
+}
